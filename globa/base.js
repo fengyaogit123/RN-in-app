009c6d99133c692f2292b8c7e37be1e3a2656864
fengyao123 <1724238345@qq.com>
2017-04-11 22:26:31 +0800

    更新 下拉刷新

--- globa/base.js
+++ globa/base.js
@@ -1,4 +1,4 @@
-export const SERVER_PATH = "http://192.168.0.104:3000"
+export const SERVER_PATH = "http://192.168.43.115:3000"
 export const IN_THEATERS = `${SERVER_PATH}/movie/in_theaters`;
 export const COMING_SOON = `${SERVER_PATH}/movie/coming_soon`;
 export const  BOOK_LIST =  `${SERVER_PATH}/book/getBookList`;
--- view/movie/index.js
+++ view/movie/index.js
@@ -1,22 +1,24 @@
-import React,{Component} from 'react';
-import{View,Text,Image,StyleSheet,ScrollView,RefreshControl} from 'react-native';
+import React,{Component,PureComponent } from 'react';
+import {View,Text,Image,StyleSheet,ScrollView,RefreshControl} from 'react-native';
 import * as Inface from "../../globa/base";
 import Title from "./Title";
 import MovieItem from './MovieItem';
 import Soon from './Soon';
 import Header from '../Component/Header';
-export default class Content extends Component{
+
+
+export default class Content extends PureComponent{
     constructor(props){
         super(props);
         this.state={
-        dataSource:[],
-        isrefreshing:false
+            dataSource:[],
+            isRefreshing:false
         };
-        
+        this._onRefresh=this._onRefresh.bind(this);
+        this.update=this.update.bind(this);
     }
     componentWillMount(){
         this.getMovies();
-        
     }
     //获取数据
     getMovies(){
@@ -26,7 +28,12 @@ export default class Content extends Component{
         .then((response)=>response.json())
         .then((data)=>{
             if(data.status==="success"){
-                this.setState(Object.assign({},this.state,{dataSource:data.subjects}));
+                requestAnimationFrame(()=>{
+                    this.update({
+                        dataSource:data.subjects,
+                        isRefreshing:false
+                    });
+                });
             }else{
                 console.log("error");
             }
@@ -35,27 +42,45 @@ export default class Content extends Component{
             console.log(error);
         })
     }
-
+    _onRefresh(){
+        this.update({isRefreshing: true});
+        this.getMovies();
+    }
+    update(newstate){
+        this.setState(Object.assign({},this.state,newstate));
+    }
     render(){
         const { navigate } = this.props.navigation; 
         return(
             <View style={{flex:1}}>
-                <Header onSearch={(text)=>{ alert(text) }} />
+                <Header onSearch={(text)=>{ alert(text) }}/>
                 <ScrollView 
-                style={{backgroundColor:"#fff"}} >
-                    <Soon horizontal={true} />
-                    <Title label="即将上映" />
-                    <View style={[style.contain,{flexWrap:"wrap"}]}>
-                        {this.state.dataSource.map((movie,index)=>(
-                            <MovieItem 
-                                title={movie.title} 
-                                uri={movie.images.large}
-                                boxStyle={{height:200}} 
-                                key={index}
-                                onSelect={()=>navigate('MovieDet', { movieId: movie.id,title:movie.title,mode:true })}
-                            />   
-                        ))}
-                    </View>
+                style={{backgroundColor:"#fff"}} 
+                refreshControl={
+                    <RefreshControl
+                        refreshing={this.state.isRefreshing}
+                        onRefresh={this._onRefresh}
+                        tintColor="#ff0000"
+                        title="Loading..."
+                        titleColor="#00ff00"
+                        colors={['#4395FF', '#76EE00', '#C0FF3E']}
+                        progressBackgroundColor="rgba(255,255,255,.9)"
+                    />
+                }
+                >
+                <Soon horizontal={true} />
+                <Title label="即将上映" />
+                <View style={[style.contain,{flexWrap:"wrap"}]}>
+                    {this.state.dataSource.map((movie,index)=>(
+                        <MovieItem 
+                            title={movie.title} 
+                            uri={movie.images.large}
+                            boxStyle={{height:200}} 
+                            key={index}
+                            onSelect={()=>navigate('MovieDet', { movieId: movie.id,title:movie.title,mode:true })}
+                        />   
+                    ))}
+                </View>
                 </ScrollView>
             </View>
         )
